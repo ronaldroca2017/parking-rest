74eab20687ef9bcaddd12c367eb7b6ffb21d26d1
Oscar Pasache <oscar@lab1505.pe>
2018-09-26 16:11:30 -0500

    test rest

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -10,8 +10,8 @@ class SearchController extends Controller
     public function index (Request $request) {
         $search = Estacionamiento::where('latitud', '<', ( $request->get('lati') + $request->get('dla') ))
                     ->where('latitud', '>', ( $request->get('lati') - $request->get('dla') ))
-                    ->where('latitud', '<', ( $request->get('longi') + $request->get('dlo') ))
-                    ->where('latitud', '>', ( $request->get('longi') - $request->get('dlo') ))
+                    ->where('longitud', '<', ( $request->get('longi') + $request->get('dlo') ))
+                    ->where('longitud', '>', ( $request->get('longi') - $request->get('dlo') ))
                     ->get();
 
         return response()->json([
